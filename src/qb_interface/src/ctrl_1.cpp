346a727b602fcc5eddf15cd91e2a5512408941e8
vignif <vignif@gmail.com>
2018-09-05 12:30:14 +0200

    usage function added, write to file zeros if no contact once, participant id as argv

--- src/qb_interface/src/ctrl_1.cpp
+++ src/qb_interface/src/ctrl_1.cpp
@@ -21,47 +21,52 @@
 #include <ctime>
 
 using namespace std;
-
+string save_file(const string& name);
+static void show_usage(std::string name);
 int main(int argc, char **argv)
 {
-	string error = "Usage: Input the HandSize code\n 1(verysmall hand)->2(verybig hand)";
-	if (argc < 2) {
+
+
+	if (argc < 3) {
 		// Tell the user how to run the program
-		std::cerr << error << std::endl;
+		show_usage(argv[0]);
 		/* "Usage messages" are a conventional way of telling the user
 		 * how to run a program if they enter the command incorrectly.
 		 */
 		return 1;
 	}
 	int q0;float p[3];
-	//	if(*argv[1]=='1'){
-	//		q0=12000;
-	//		p[0]=-0.01124; p[1]=2.061; p[2]=4.864; p[3]=-4210;
-	//	}else if(*argv[1]=='2'){
-	////		PROBLEM HERE
-	//		q0=11000;
-	//		p[0]=0.475775; p[1]=-34.546; p[2]=297.2; p[3]=-5854.5;
-	//	}else
+
 	if(*argv[1]=='1'){
+		q0=8500;
+		p[0]=0.02538; p[1]=-3.471; p[2]=177.5; p[3]=0;
+	}else if(*argv[1]=='2'){
+		q0=9000;
+		p[0] = 0.02432; p[1] =-2.862; p[2]= 157.2; p[3]=0;
+	}else if(*argv[1]=='3'){
+		q0=10000;
+		p[0]=0.0232; p[1]=-2.1; p[2]=92.15; p[3]=0;
+	}else if(*argv[1]=='4'){
+		q0=11000;
+		p[0]=0.019435; p[1]=-3.7; p[2]=270.15; p[3]=0;
+	}else if(*argv[1]=='5'){
 		q0=12000;
 		p[0]=0.007035; p[1]=-0.1305; p[2]=55.82; p[3]=0;
-	}else if(*argv[1]=='2'){ //BETTER ONE
-		q0=11000;
-		p[0]=0.019435; p[1]=-3.7; p[2]=276.15; p[3]=0;
-		//	}else if(*argv[1]=='5'){
-		//		q0=7000;
 
+
+		// AVG(q0) = 10100
 	}else {
-		std::cerr << error << std::endl;
-
+		show_usage(argv[0]);
+		cout << "handsize code out of bounds" << std::endl;
 		return 1;
 	}
+	string id_participant = argv[2];
 
-
-	cout << "initial position set to " << q0 <<endl;
+	cout <<"Participant ID = "<< id_participant <<"\ninitial position set to " << q0 <<endl;
 	int q;
 	ros::init(argc, argv, "ctrl_1");
 	ros::NodeHandle n;
+	ros::Rate loop_rate(1000); //Hz
 	ros::Subscriber sub = n.subscribe("sensors_FSR", 100, arrayCallback_sensors);
 	ros::Publisher pub = n.advertise<qb_interface::handRef>("/qb_class/hand_ref", 100);
 	qb_interface::handPos state;
@@ -74,46 +79,72 @@ int main(int argc, char **argv)
 	ros::Subscriber sub2 = n.subscribe("/qb_class/hand_measurement",100, &callbacks::cb_current, &cb);
 	ros::Subscriber sub3 = n.subscribe("sensors_FSR", 100, arrayCallback_sensors);
 
-	string dir = "/home/francesco/ros_ws_handshake/ctrl/1/";
-	string name = "experiment";
-	string filename = dir + name;
-	int idx=0;
-	string filetosave;
-	bool found = false;
-	for (int idx=0; idx<10; idx++){
-		if (!fexists(filename + std::to_string(idx) + ".csv"))
-		{
-			filetosave=filename + std::to_string(idx) + ".csv";
-			break;
-		}
-	}
+	// Saving File routine
+	string name = "id_" + id_participant + "_n";
+	//save_file checks if a file exists in a specific folder
+	//it true create a new file with a incremental number in the name
 
+	std::ofstream outFile;
+	cout << "saving file: " << save_file(name) << endl;
+	outFile.open(save_file(name));
 
-	std::ofstream outFile(filetosave);
-
+	bool last_contact= false ;
 	while (ros::ok())
 	{
 		bool in_contact= check_contact();
 		state.closure.clear();
 		if(in_contact){
-			//			cout << "IN contact" << endl;
+
+			//  in contact
 			float sumofFSR=0;
 
-			//q=compute_f(sumofFSR);
 			q=compute_f_with_q0(sumofFSR,q0,p);
 			outFile << Arr[0] << ", " << Arr[1]<< ", "<< Arr[2] << ", " << cb.closure << ", "<< cb.current <<endl;
-			//q=compute_f_francesco(sumofFSR);
+
 			state.closure.push_back(q); //round the closure value to the closest integer
-			//n.setParam("/stiffness",0.9); //publish parameter to ros
 
 		}else{
-			//			cout << "NOT contact" << endl;
+			// not in contact
+			if(last_contact!=in_contact)
+			outFile << 0 << ", " << 0<< ", "<< 0 << ", " << 0 << ", "<< 0 <<endl;
 			state.closure.push_back((int) 0 );
 		}
 		pub.publish(state);
 		ros::spinOnce();
-		usleep(1000);  //dynamic usleeps takes microseconds in input
-
+//		usleep(1000);  //dynamic usleeps takes microseconds in input
+		loop_rate.sleep();
+		last_contact=in_contact;
 	}
+	outFile.close();
+
 }
 
+
+
+
+
+string save_file(const string& name) {
+	/*save_file checks if a file exists in a specific folder
+	it true create a new file with a incremental number in the name
+	 */
+	string dir = "/home/francesco/ros_ws_handshake/ctrl/1/";
+	string filename = dir + name;
+	int idx = 0;
+	string filetosave;
+	bool found = false;
+	for (int idx = 0; idx < 10; idx++) {
+		if (!fexists(filename + std::to_string(idx) + ".csv")) {
+			filetosave = filename + std::to_string(idx) + ".csv";
+			break;
+		}
+	}
+	return filetosave;
+}
+
+static void show_usage(std::string name){
+	std::cerr << "Usage:\n"
+			<< "ctrl_1 [handsize_code] [ID_participant]\n"
+			<< "Usage: Input the HandSize code\n"
+			<< "1(verybig hand)->5(verysmall hand)\n"
+			<< std::endl;
+}
